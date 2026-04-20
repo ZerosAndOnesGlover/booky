47091a44d1c1976333973ee4990cc069ba590c4a
Adebayo Glover <zerosandones.glover@gmail.com>
2026-04-20 11:39:46 +0100

    walahi, final commit seh

--- server/server.js
+++ server/server.js
@@ -38,7 +38,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Device-ID'],
 }));
 
 // --- Body Parsing Middleware ---
